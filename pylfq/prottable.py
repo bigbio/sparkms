f3917cb08d8e197d52822bde8b6eeaabef34de1c
yperez <ypriverol@gmail.com>
2020-11-12 16:15:57 +0000

    first data structure

--- pylfq/prottable.py
+++ pylfq/prottable.py
@@ -1,24 +1,14 @@
-HEADER_PROTEIN = 'Protein ID'
-HEADER_PROTEINS = 'Protein ID(s)'
-HEADER_GENEID = 'Gene ID'
-HEADER_GENENAME = 'Gene Name'
-HEADER_DESCRIPTION = 'Description'
-HEADER_COVERAGE = 'Coverage'
-HEADER_NO_PROTEIN = '# Proteins'
-HEADER_CONTENTPROT = 'Proteins in group'
-HEADER_NO_UNIPEP = '# Unique peptides'
-HEADER_NO_PEPTIDE = '# Peptides'
-HEADER_NO_PSM = '# PSMs'
-HEADER_NO_PSMS_SUFFIX = ' - # quanted PSMs'
-HEADER_NO_FULLQ_PSMS = 'Amount fully quanted PSMs'
-HEADER_AREA = 'MS1 precursor area'
+HEADER_PROTEIN = 'protein id'
+HEADER_PROTEINS = 'protein group ids'
+HEADER_GENEID = 'gene'
+HEADER_GENENAME = 'gene name'
+HEADER_DESCRIPTION = 'description'
+HEADER_COVERAGE = 'coverage'
+HEADER_NO_UNIPEP = '# unique peptides'
+HEADER_NO_PEPTIDE = '# peptides'
+HEADER_NO_PSM = '# psms'
+HEADER_AREA = 'intensity'
 HEADER_QVAL = 'q-value'
 HEADER_PEP = 'PEP'
-HEADER_QVAL_MODELED = 'q-value (linear model)'
-HEADER_QSCORE = 'Q-score best peptide'
-PICKED_HEADER = [HEADER_QSCORE, HEADER_QVAL]
-ACCESSIONS = {
-        'prottable': HEADER_PROTEIN,
-        'genetable': HEADER_GENEID,
-        'associdtable': HEADER_GENENAME,
-        }
+HEADER_SAMPLES = "samples"
+HEADER_IBAQ = "ibaq"
