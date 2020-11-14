2faa9062228212d2d83edb4cf21f0d374c8928e5
yperez <ypriverol@gmail.com>
2020-11-14 13:50:29 +0000

    first data structure

--- sparkms/commons/peptable.py
+++ sparkms/commons/peptable.py
@@ -1,5 +1,11 @@
+# Peptide information
 HEADER_PEPTIDE = 'peptide sequence'
 HEADER_NO_PSM = '# psms'
+HEADER_PROTEIN = 'protein id'
+HEADER_GENE = 'gene'
+HEADER_GENE_NAME = 'gene name'
+
+# Mass spec information
 HEADER_INTENSITY = 'intensity'
 HEADER_ID_SCORE = 'id score'
 HEADER_MS_RUN = 'ms-run'
@@ -7,11 +13,10 @@ HEADER_MS_SCAN = 'ms-scan'
 HEADER_MS_RT = "rt"
 HEADER_MS_CHARGE = "charge"
 HEADER_IS_DECOY = 'isdecoy'
-HEADER_PROTEIN = 'protein id'
-HEADER_GENE = 'gene'
-HEADER_GENE_NAME = 'gene name'
+
+# Sample information
 HEADER_SAMPLE_ID = 'sample iD'
-HEADER_ORGANISM  = 'px accession'
+HEADER_PX_PROJECT_ACCESSION  = 'px accession'
 HEADER_ORGANISM = 'organism'
 HEADER_ORGANISM_PART = 'organism part'
 HEADER_DISEASE = 'disease'
